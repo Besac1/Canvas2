d71f1710d3e60a82eb539d2dbfa4845c009a69be
Chollet Axel <cholletax@orange.fr>
2015-08-25 09:11:12 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Canvas</title>
+    <link rel = "stylesheet" type = "text/css" href = "CSS/style.css"/>
+    <meta charset = 'UTF-8'/>
+    <script data-main = "JS/script" src = "JS/require.js"></script>
+
+</head>
+
+<body>
+
+    <div id="viewport"></div>
+    
+    <script type = "text/javascript" src = 'JS/script.js'></script>
+
+</body>
+
+</html>
